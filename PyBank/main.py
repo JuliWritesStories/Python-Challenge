435b1440335c7f8916b9f047f14815b877ca780d
JuliWritesStories <julietmm566@gmail.com>
2023-11-05 13:24:57 -0800

    step 2 of homework

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+#import csv for banking data
+#csv has two columns: Date and Profit/Losses
+#find the total number of months in the data set
+#find the net amount of profits/losses over the entire period
+#find the average change
+#find the greatest increase in profits with date and amount
+#find the greatest decrease in profits with date and amount
+#print and export into a text file
---
+++ PyPoll/main.py
@@ -0,0 +1,8 @@
+#import the csv for Poll data
+#dataset has three columns: Voter ID, County, and Candidate
+#find the total number of votes cast
+#create a complete list of candidates who recieved votes
+#find the percentage of votes each candidate won
+#find the total number of votes each candidate won
+#find the winner of the election based on popular vote
+#Print results in terminal and export to text file
