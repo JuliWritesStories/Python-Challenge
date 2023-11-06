cd88e2a3c4a072d22b9af92af6aae3ec7c8d5b23
JuliWritesStories <julietmm566@gmail.com>
2023-11-06 11:16:36 -0800

    step 3 of hw

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,26 @@
 #import csv for banking data
-#csv has two columns: Date and Profit/Losses
+import os
+import csv
+#set the path and read the csv
+budget_file = os.path.join('Resources','budget_data.csv')
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+with open(budget_file, encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile)
+#declare columns
+Date = []
+Profit = []
+#declare future values
+total_months = 0
+total_profit = 0
+Average_change = 0
+Greatest_increase = 0
+Greatest_decrease = 0
+
 #find the total number of months in the data set
 #find the net amount of profits/losses over the entire period
 #find the average change
 #find the greatest increase in profits with date and amount
 #find the greatest decrease in profits with date and amount
 #print and export into a text file
+print("Financial Analysis")
+print("----------------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,11 @@
 #import the csv for Poll data
+import os
+import csv
+#read the csv
+Poll_file = os.path.join('Resources','election_data.csv')
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+with open(Poll_file, encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile)
 #dataset has three columns: Voter ID, County, and Candidate
 #find the total number of votes cast
 #create a complete list of candidates who recieved votes
